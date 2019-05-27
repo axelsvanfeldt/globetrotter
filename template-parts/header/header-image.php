86e995cda22f7318c278822f997a365bd8481e4a
axel <axel.svanfeldt@gmail.com>
2019-05-28 00:38:14 +0200

    Added conditional statement to display archive titles

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -27,13 +27,15 @@ else if (is_single() && !is_attachment()) {
 else {
     $slug = get_post_field('post_name', get_post());
     if ($slug != 'planned-trips' && $slug != 'places-visited'):
-        if (get_header_image()): ?>
+        if (get_header_image()): 
+            $title = is_archive() ? single_month_title(' ', FALSE) : get_the_title();
+            ?>
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12 py-0 px-0 header-image" style="background-image:url(<?php header_image(); ?>); height: <?php echo absint(get_custom_header()->height); ?>px; color:#<?php echo get_header_textcolor();?>">
                         <h1 class="mt-5 font-weight-bold"><?php echo get_bloginfo('name'); ?></h1>
                         <div class="pb-2 px-3 header-description" style="border-color:#<?php echo get_header_textcolor();?>"><?php echo get_bloginfo('description'); ?></div>
-                        <h5 class="mt-5 font-weight-bold"><?php echo get_the_title(); ?></h5>
+                        <h5 class="mt-5 font-weight-bold"><?php echo $title; ?></h5>
                         <?php get_template_part('template-parts/header/header', 'sharing'); ?>
                     </div>
                 </div>
