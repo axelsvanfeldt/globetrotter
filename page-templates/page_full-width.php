20e6b87224413330d6a95698ba340371a9d675d3
axel <axel.svanfeldt@gmail.com>
2019-05-28 00:18:56 +0200

    Added the loop and comments template

--- page-templates/page_full-width.php
+++ page-templates/page_full-width.php
@@ -5,8 +5,14 @@ get_header();?>
 <div class="container-fluid">
     <div clasS="row">
         <div class="col-12 py-0 px-0">
-            <?php globetrotter_get_template('template-parts/content/content', 'page'); ?>
+            <?php while (have_posts()) {
+				the_post();
+                the_content();
+            } ?>
         </div>
     </div>
 </div>
-<?php get_footer(); ?>
+<?php 
+comments_template();
+get_footer();
+?>
--- page.php
+++ page.php
@@ -1,9 +1,15 @@
-<?php get_header();?>
+<?php get_header(); ?>
 <div class="container">
     <div clasS="row">
         <div class="col-12 py-4 px-4">
-            <?php globetrotter_get_template('template-parts/content/content', 'page'); ?>
+            <?php while (have_posts()) {
+				the_post();
+                the_content();
+            } ?>
         </div>
     </div>
 </div>
-<?php get_footer(); ?>
+<?php 
+comments_template();
+get_footer();
+?>
