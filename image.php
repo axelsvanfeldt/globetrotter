5b530d83de5562c99aed2839bd6a2f6b24a5f629
axel <axel.svanfeldt@gmail.com>
2019-05-28 15:43:51 +0200

    Moved content from template-parts to image.php

--- image.php
+++ image.php
@@ -1,6 +1,20 @@
 <?php get_header(); ?>
 <div class="container">
-    <?php globetrotter_get_template('template-parts/content/content', 'image'); ?>
+    <?php if (have_posts()) {
+        the_post(); ?>
+        <div class="row">
+            <div class="col-12 py-2 px-2 py-sm-4 px-sm-4 single-image-wrapper">
+                <?php echo wp_get_attachment_image(get_the_ID(), 'large-image', false, array("class" => "single-image mb-4")); ?>
+                <div class="col-12 text-center">
+                    <h6>Published <?php echo globetrotter_time_ago(); ?> by <?php the_author(); ?></h6>
+                </div>
+            </div>
+        </div>
+    <?php 
+    }
+    else {
+        get_template_part('template-parts/content/content', 'notfound');
+    } ?>
 </div>
 <?php
 comments_template();
