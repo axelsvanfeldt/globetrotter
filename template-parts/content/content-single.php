ff86385028a1d61f01850baaab15a9970113b592
axel <axel.svanfeldt@gmail.com>
2019-05-28 02:55:11 +0200

    Removed comments template

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,8 +1,5 @@
-<div class="container bg-white">
-    <div class="row py-3 px-2 px-sm-4 py-sm-5">
-        <div class="col-12">
-            <?php the_content(); ?>
-        </div>
+<div class="row py-3 px-2 px-sm-4 py-sm-5">
+    <div class="col-12">
+        <?php the_content(); ?>
     </div>
 </div>
-<?php comments_template(); ?>
