ae63d1e76c0124b9bd3169c907b289ea0b9ca59a
axel <axel.svanfeldt@gmail.com>
2019-05-28 02:52:06 +0200

    Removed content wrapper

--- template-parts/content/content-image.php
+++ template-parts/content/content-image.php
@@ -1,12 +1,9 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12 py-2 px-2 py-sm-4 px-sm-4 single-image-wrapper">
-            <?php echo wp_get_attachment_image(get_the_ID(), 'large-image', false, array("class" => "single-image mb-4")); ?>
-            <div class="col-12 text-center">
-                <?php //globetrotter_render_post_tags(get_the_ID()); ?>
-                <h6>Published <?php echo globetrotter_time_ago(); ?> by <?php the_author(); ?></h6>
-            </div>            
-        </div>
+<div class="row">
+    <div class="col-12 py-2 px-2 py-sm-4 px-sm-4 single-image-wrapper">
+        <?php echo wp_get_attachment_image(get_the_ID(), 'large-image', false, array("class" => "single-image mb-4")); ?>
+        <div class="col-12 text-center">
+            <?php //globetrotter_render_post_tags(get_the_ID()); ?>
+            <h6>Published <?php echo globetrotter_time_ago(); ?> by <?php the_author(); ?></h6>
+        </div>            
     </div>
 </div>
-<?php comments_template(); ?>
